69114c810a6c09cf509471f31c3279591e90658f
Sondre Bjarkum <43411106+sondrebjarkum@users.noreply.github.com>
2022-08-06 10:37:04 +0200

    git testing

--- src/controllers/post/createSauce.js
+++ src/controllers/post/createSauce.js
@@ -1,13 +1,34 @@
 const simpleGit = require("simple-git");
 const git = simpleGit.default();
 
+const addRemoteRepository = async () => {
+    // await git.checkout("testCheckout");
+    // const branch = await git.branch();
+    // console.log(branch.current)
+    try{
+        await git.addRemote('sauces', "https://github.com/sondrebjarkum/sondrebjarkum.github.io")
+        
+    }catch{
+        console.log("Remote already exists!")
+    }
+    // const remote = await git.listRemote()
+    // console.log(remote)
+}
+
+const pushToRemote = async () => {
+    const remotes = await git.getRemotes(true)
+    const filtered = remotes.filter( e => e.name === 'sauces').map( e => e.refs.push)
+    console.log(filtered)
+    git.push(filtered)
+}
 
 exports.createSauce = async (req, res, next) => {
     console.log("Create sauce controller fired...")
-    console.log(req.body)
+    // console.log(req.body)
 
-    const sauceData = {}
-
+    addRemoteRepository()
+    pushToRemote()
+    
     res.status(200).json({
         type: "success",
         message: "Post created successfully",
@@ -15,4 +36,5 @@ exports.createSauce = async (req, res, next) => {
         // post,
         },
     });
+    
 }
