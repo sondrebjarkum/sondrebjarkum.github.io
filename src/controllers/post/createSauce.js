d2157e4ccf0a99e4c320fd9b3f30f6d998bb7596
Sondre Bjarkum <43411106+sondrebjarkum@users.noreply.github.com>
2022-08-06 17:13:16 +0200

    working?

--- src/controllers/post/createSauce.js
+++ src/controllers/post/createSauce.js
@@ -17,7 +17,7 @@ const addRemoteRepository = async () => {
     // const remote = await git.listRemote()
     // console.log(remote)
 }
-const pushToBranch = async () => {
+const pushToBranch = async (resolve, reject) => {
     console.log("pushToBranch")
     
     try{
@@ -25,28 +25,31 @@ const pushToBranch = async () => {
         // git.commit("Update sauces", "sauces").push('origin', 'testing');
         git.add(["./data/sauces.json"])
             .commit("Update sauces")
-            .push(['-u', 'origin', 'testing'], () => console.log('push done'));
+            .push(['-u', 'origin', 'testing'], () => resolve("Git done...!"));
     }catch(e){
         console.log(e)
+        reject(new Error('Something is not right in git!'))
     }
     console.log("Git done")
 
 }
-const writeFile = async () => {
+const writeFile = async (obj) => {
     console.log("Getting sauces file...")
+    console.log(obj)
+    return new Promise( (resolve, reject) => {
 
     var result = JSON.parse(fs.readFileSync('./data/sauces.json', 'utf8'))
-    const testObject = {
-        brand: 'Sondresaus',
-        name: 'Blåbær',
-        strength: '7',
-        price: '169',
-        descr: 'Sterk-- saus som smaker mye av blåbær. Morsom på pannekaker, fin i gryter og sauser.',
-        imageurl: 'ssblåbær.jpg',
-        tags: [ 'blåbær' ],
-        chilis: [ 'Ghost' ]
-    }
-    result.push(testObject)
+    // const testObject = {
+    //     brand: 'Sondresaus',
+    //     name: 'Blåbær',
+    //     strength: '7',
+    //     price: '169',
+    //     descr: 'Sterk-- saus som smaker mye av blåbær. Morsom på pannekaker, fin i gryter og sauser.',
+    //     imageurl: 'ssblåbær.jpg',
+    //     tags: [ 'blåbær' ],
+    //     chilis: [ 'Ghost' ]
+    // }
+    result.push(obj)
     
     const filter = result.filter( (e) => e.name === "Blåbær")
     // console.log(filter)
@@ -54,30 +57,61 @@ const writeFile = async () => {
     console.log("Writing to file...")
 
     fs.writeFile("./data/sauces.json", JSON.stringify(result), (err) => {
-        if (err)
+        if (err){
             console.log(err);
+            reject(new Error('Something is not right in file writing!'))
+        }
         else {
             console.log("File written successfully\n");
-            pushToBranch()
+            // pushToBranch()
+            resolve("File written successfully")
         }
     })
+
+    })
+    
 }
 
 exports.createSauce = async (req, res, next) => {
-    console.log("Create sauce controller fired...")
+    // console.log("Create sauce controller fired...")
 
-    writeFile()
-    pushToBranch()
+    // writeFile()
+    // pushToBranch()
+
+    // let prom1 = new Promise(writeFile)
+    //     .then(new Promise(pushToBranch).then(() => {
+    //         res.status(200).json({
+    //             type: "success",
+    //             message: "Post created successfully",
+    //             data: {
+    //             // post,
+    //             },
+    //         });
+    //     }) )
+    const obj = req.body
+    writeFile(obj)
+    .then(new Promise(pushToBranch).then(() => {
+        res.status(200).json({
+            type: "success",
+            message: "Sauce has been added successfully!",
+            data: {
+                obj,
+            },
+        });
+    }) )
+
+    // let prom1 = new Promise(writeFile)
+    // .then(new Promise(pushToBranch).then(() => {
+    //     res.status(200).json({
+    //         type: "success",
+    //         message: "Post created successfully",
+    //         data: {
+    //         // post,
+    //         },
+    //     });
+    // }) )
 
     
-
-    await res.status(200).json({
-        type: "success",
-        message: "Post created successfully",
-        data: {
-        // post,
-        },
-    });
 
     
     
